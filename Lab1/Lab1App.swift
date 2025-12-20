e73133a79201f91b1df7472511e2d662b6358c95
Artem Kukliak <23kukliak.a@nltu.lviv.ua>
2025-12-20 20:33:11 +0200

    Initial Commit

---
+++ Lab1/Lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1App.swift
+//  Lab1
+//
+//  Created by User on 20.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab1Tests/Lab1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1Tests.swift
+//  Lab1Tests
+//
+//  Created by User on 20.12.2025.
+//
+
+import Testing
+@testable import Lab1
+
+struct Lab1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
